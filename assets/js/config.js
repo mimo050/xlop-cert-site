94ec5d008ae5d04d608dd009fd3090056615d921
mimo050 <78521557+mimo050@users.noreply.github.com>
2025-08-24 22:53:28 +0300

    Handle missing GBOX base URL

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,9 +3,15 @@ export const BACKEND_URL = 'https://xlop-cert-site.onrender.com';
 export const GBOX_BASE = '';
 
 export function gboxLink({ udid, token, redirect }) {
+  if (!GBOX_BASE) {
+    console.error('GBOX_BASE is not configured');
+    return '#';
+  }
+
   const params = new URLSearchParams();
   if (udid) params.set('udid', udid);
   if (token) params.set('token', token);
   if (redirect) params.set('redirect', redirect);
+
   return `${GBOX_BASE}?${params.toString()}`;
 }
