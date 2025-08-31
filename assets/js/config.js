c26a1548972e72dffc423422b7b7184bcc7644b0
mimo050 <78521557+mimo050@users.noreply.github.com>
2025-08-31 16:43:11 +0300

    feat(udid): add mobileconfig flow + optional Paymob env; autofill UDID and fix minor JS

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,17 +1,2 @@
-export const FRONT_URL = 'https://mimo050.github.io/xlop-cert-site';
-export const BACKEND_URL = 'https://xlop-cert-site.onrender.com';
-export const GBOX_BASE = 'https://gbox.example.com/install';
-
-export function gboxLink({ udid, token, redirect }) {
-  if (!GBOX_BASE) {
-    console.error('GBOX_BASE is not configured');
-    return '#';
-  }
-
-  const params = new URLSearchParams();
-  if (udid) params.set('udid', udid);
-  if (token) params.set('token', token);
-  if (redirect) params.set('redirect', redirect);
-
-  return `${GBOX_BASE}?${params.toString()}`;
-}
+export const FRONT_URL   = 'https://mimo050.github.io/xlop-cert-site';
+export const BACKEND_URL = 'https://xlop-cert-backend.onrender.com'; // عدّل الاسم إذا اختلف على Render
